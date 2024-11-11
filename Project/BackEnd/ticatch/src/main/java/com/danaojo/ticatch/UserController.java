67b2999a0db750be05701fbf2dafe5f2797f7da6
KMS <dkimdaniel0911@gmail.com>
2024-11-11 14:05:26 +0900

    back

--- Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/UserController.java
+++ Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/UserController.java
@@ -1,11 +1,13 @@
 package com.danaojo.ticatch;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
-	@GetMapping("/api/data")
+	@GetMapping("/data")
 	public String getData() {
 		return "Hello from Spring Boot!";
 	}
