6a684912895c7c61f68c15b5bd1c6ae2d9da8848
sw2park <pswoo990803@gmail.com>
2024-11-11 15:05:40 +0900

    24.11.11 sync (#21)

--- Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/controller/HelloController.java
+++ Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/controller/HelloController.java
@@ -1,15 +1,13 @@
-package controller;
+package com.danaojo.ticatch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/api/hello")
     public String test() {
-        return "Hello, world!";
+    	System.out.println("응답");
+        return "srping hello";
     }
-
-
 }
