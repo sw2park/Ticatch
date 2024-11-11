9589e95f3a7464d2288718f37ed13ba7badcd3bf
KMS <dkimdaniel0911@gmail.com>
2024-11-11 13:42:54 +0900

    back

--- Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/UserController.java
+++ Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/UserController.java
@@ -1,11 +1,9 @@
 package com.danaojo.ticatch;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:5173") // Allow requests from Vite
 public class UserController {
 	@GetMapping("/api/data")
 	public String getData() {
