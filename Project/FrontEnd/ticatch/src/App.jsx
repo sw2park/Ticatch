72cfa99eac81203553bc7796ef914bb7961b5abb
syc0708 <tim0708@naver.com>
2024-11-26 16:27:49 +0900

    20241126 헤더푸터 작성

--- Project/FrontEnd/ticatch/src/App.jsx
+++ Project/FrontEnd/ticatch/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios';
+import Footer from './component/footer/Footer.jsx'
+import './component/footer/Footer.jsx'
+import Header from './component/header/Header.jsx';
+
 
 function App() {
   const [data, setData] = useState('')
@@ -16,6 +20,8 @@ function App() {
    return (
        <div>
            받아온 값 : {data}
+           <Header></Header>
+           <Footer></Footer>
        </div>
    );
 }
