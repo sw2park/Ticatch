1d66b58ece369985df55df5f7675929637802f28
sw2park <pswoo990803@gmail.com>
2024-11-28 14:17:14 +0900

    Ft api (#32)
    * 24.11.19 api 공연 목록, 공연 상세dto 작업
    
    * 24.11.21 jpa 사용
    
    * 24.11.22 Kopis api 받아온 후 JPA활용하여 DB저장(performList, performDetail)
    
    * 24.11.25 PFJoinDB 작업 중간 완료
    
    * 24.11.25 쿼리 추가
    
    * 24.11.26 Kopis Api 상세이미지 제외
    
    * 24.11.26 상세 이미지 작업중
    
    * 24.11.26 API 상세이미지 추가
    
    * 24.11.28 api 수정

--- Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/api/ApiTestMain.java
+++ Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/api/ApiTestMain.java
@@ -1,5 +1,6 @@
 package com.danaojo.ticatch.api;
 
+import com.danaojo.ticatch.api.kopis.Kopis;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
