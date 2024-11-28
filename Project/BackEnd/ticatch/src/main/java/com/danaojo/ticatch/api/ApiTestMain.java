46837d2e1b2ec03767b87393d4b034d0fb0ab269
sw2park <pswoo990803@gmail.com>
2024-11-28 14:14:39 +0900

    24.11.28 api 수정

--- Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/api/ApiTestMain.java
+++ Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/api/ApiTestMain.java
@@ -1,10 +1,16 @@
 package com.danaojo.ticatch.api;
 
+import com.danaojo.ticatch.api.kopis.Kopis;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class ApiTestMain {
 	public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
-		
+		Kopis kopis = new Kopis();
+		String id = "PF254755";
+//		kopis.callConcertList();
+//		kopis.callConcertDetail(id);
+		String name = "FC001247";
+		System.out.println(kopis.callFacilityDetail(name));
 	}
 }
