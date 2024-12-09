091a7eb8953e9e388a3e5366d766db3987e7fd26
son <seokyyy_@naver.com>
2024-12-09 17:29:41 +0900

    24.12.09 로그인 어느정도 살린거

--- Project/FrontEnd/ticatch/src/App.jsx
+++ Project/FrontEnd/ticatch/src/App.jsx
@@ -1,53 +1,18 @@
-<<<<<<< HEAD
-import {useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios';
-import Footer from './component/footer/Footer.jsx'
-import './component/footer/Footer.jsx'
-import Header from './component/header/Header.jsx';
-
-=======
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import SeatBooking from "./Seats/SeatBooking.jsx";
-import Order from "./Orders/Order.jsx";
->>>>>>> 3056983a26899a56f4d46e38736e8c91ca543f6b
+import axios from "axios";
+import Footer from "./component/footer/Footer.jsx";
+import Header from "./component/header/Header.jsx";
+import Login from "./signUp/login.jsx";
 
 const App = () => {
-  const [selectedSeats, setSelectedSeats] = useState([]);
-  const [noSeatInfo, setNoSeatInfo] = useState(null); // 공통 상태 생성
-
   return (
-    <div className="app-container">
-      <div className="left">
-        <SeatBooking
-          selectedSeats={selectedSeats}
-          setSelectedSeats={setSelectedSeats}
-          noSeatInfo={noSeatInfo}
-        />
-      </div>
-      <div className="right">
-        <div className="r r1">
-          <Order selectedSeats={selectedSeats} setNoSeatInfo={setNoSeatInfo} />
-        </div>
-      </div>
+    <div>
+      <Header></Header>
+      <Login />
+      <Footer></Footer>
     </div>
   );
 };
 
-<<<<<<< HEAD
-   return (
-       <div>
-           받아온 값 : {data}
-           <Header></Header>
-           <Footer></Footer>
-       </div>
-   );
-}
-
 export default App;
-=======
-export default App;
->>>>>>> 3056983a26899a56f4d46e38736e8c91ca543f6b
