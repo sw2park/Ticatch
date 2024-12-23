9016d1d75be6f8b190d9462da40afb60e0ed031e
Daniel <dkimdaniel0911@gmail.com>
2024-12-23 12:06:08 +0900

    오류 다 해결한듯

--- Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/order/Entity/SaveDTO.java
+++ Project/BackEnd/ticatch/src/main/java/com/danaojo/ticatch/order/Entity/SaveDTO.java
@@ -11,16 +11,19 @@ public class SaveDTO {
 	private int seqsaveid;
 	private String userid;
 	private Long seqpfjoinid;
+	// 이거 추가하니 오류 난다?
+//	private String ppposter;
 	
 	public SaveDTO() {}
 
-	public SaveDTO(int seqsaveid, String userid, Long seqpfjoinid) {
+	public SaveDTO(int seqsaveid, String userid, Long seqpfjoinid
+//			,String ppposter
+			) {
 		super();
 		this.seqsaveid = seqsaveid;
 		this.userid = userid;
 		this.seqpfjoinid = seqpfjoinid;
+//		this.ppposter = ppposter;
 	}
 
-	
-	
 }
